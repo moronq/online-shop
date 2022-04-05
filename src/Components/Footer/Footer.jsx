bd777b170a921e1567900d14ec228c17f73c7710
moronq <moronqq@gmail.com>
2022-04-05 23:50:32 +0300

    implemented adaptive footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './Footer.module.scss';
 import FooterList from "./FooterList";
+import phone from '../../img/icons/footer-phone.svg'
+import clock from '../../img/icons/clock.svg'
+import location from '../../img/icons/footer-location.svg'
+import mail from '../../img/icons/mail.svg'
 
 const Footer = () => {
     return (
@@ -12,6 +16,13 @@ const Footer = () => {
                     <FooterList title={'СЛУЖБА ПОДДЕРЖКИ'} list={['Контактная информация', 'Возврат товара', 'Карта сайта']}/>
                     <FooterList title={'ДОПОЛНИТЕЛЬНО'} list={['Подарочные сертификаты', 'Партнеры', 'Товары со скидкой']}/>
                     <FooterList title={'ЛИЧНЫЙ КАБИНЕТ'} list={['Личный кабинет', 'История заказов','Мои закладки', 'Рассылка новостей']}/>
+                </section>
+                <section className={styles.footerFirstFloor}>
+                    <FooterList title={'контакты'} list={['8 (800) 777-49-67', `Пн-Пт 7:00 - 16:00 (МСК)`, 'Златоуст, ул. Шоссейная, д. 1, офис «6Б»', 'info@zlatmax.ru']} image = {[phone,clock,location,mail]}/>
+                    <FooterList title={'ПОЛЕЗНЫЕ ССЫЛКИ'} list={['Способы оплаты и доставки']}/>
+                    <FooterList title={'НАША ГАРАНТИЯ'} list={['Недовольны своей покупкой? Вы можете вернуть ее ' +
+                    'в течении 30 дней с даты получения, согласно нашим правилам']}/>
+                    <FooterList title={'НОВОСТНАЯ РАССЫЛКА'} list={['Подпишитесь прямо сейчас!']}/>
                 </section>
             </div>
         </footer>
--- src/Components/Footer/FooterList.jsx
+++ src/Components/Footer/FooterList.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./Footer.module.scss";
+import arrowBottomWhite from '../../img/icons/arrow-bottom-white.svg'
 
-const FooterList = ({title, list}) => {
+const FooterList = ({title, list, image}) => {
+
+    let [isSpoilerActive, setIsSpoilerActive] = useState(false)
+
+    let onSpoilerClick =()=>{
+        setIsSpoilerActive(prev => !prev)
+    }
+
     return (
         <div className={styles.footerListContainer}>
-            <p className={styles.footerListTitle}>{title}</p>
-            <ul className={styles.footerList}>
-                {list.map(el => <li className={styles.footerListItem}><a className={styles.footerListItemLink} href="">
-                    {el}</a></li>
-                )}
-            </ul>
+            <p className={styles.footerListTitle}>{title}
+                <img className={`${styles.spoilerIcon} ${isSpoilerActive ? styles.spoilerIconFlipped : ''}`} src={arrowBottomWhite} alt=''/>
+                <button onClick={onSpoilerClick} className={styles.spoilerButton}/>
+            </p>
+            <div className={styles.footerDesktop}>
+                <ul className={styles.footerList}>
+                    {list.map((el, index) => <li className={styles.footerListItem}>{image ? <img className={styles.footerImage} src={image[index]} alt={'logo'}/>  : ''}<a className={styles.footerListItemLink} href="">
+                        {el}</a></li>
+                    )}
+                </ul>
+            </div>
+            <div className={styles.footerMobile}>
+                <ul className={`${styles.footerList} ${isSpoilerActive ? '' : styles.footerListHidden}`}>
+                    {list.map((el, index) => <li className={styles.footerListItem}>{image ? <img className={styles.footerImage} src={image[index]} alt={'logo'}/>  : ''}<a className={styles.footerListItemLink} href="">
+                        {el}</a></li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
